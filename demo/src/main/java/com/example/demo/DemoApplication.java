59b41e8f1d0f4c0b15ebbf2b3b0f173f31d4d0bc
myName <me@mydomain.com>
2017-09-19 21:46:06 -0400

    Initial commit of demo project to GIT repo

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+/*
+ * This is a comment
+*/
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
